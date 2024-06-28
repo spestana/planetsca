9bd98fc3eb87a6747e0207ca56dfdb2aecfbb807
IanChiu-333 <ianchiu333@gmail.com>
2024-06-28 13:18:33 -0700

    Finished Splitting data_labeling() into 2 functions. Still needs to be tested.

--- src/planetsca/data_preparation.py
+++ src/planetsca/data_preparation.py
@@ -5,6 +5,8 @@ import pandas as pd
 import rasterio
 from rasterio import features
 from rasterio.enums import MergeAlg
+from pandas import DataFrame
+import geopandas as gpd
 
 warnings.filterwarnings("ignore")
 
@@ -51,43 +53,59 @@ def vector_rasterize(dir_vector, dir_raster, dir_out, flag_output):
     return rasterized
 
 
-def data_labeling(
-    dir_ROI, dir_raster, dir_ROIraster, dir_samples_root, dir_samples
-):  # Not sure what to call this function
-    flag_rasterize = False
-    # FILE PATHS CHANGE DEPENDING ON THE PACKAGE - THESE ARE OLD
-    # dir_ROI = "./data/ROI/20180528_181110_add_UTM11.shp"
-    # dir_raster = "./data/planet/train/20180528_181110_1025_3B_AnalyticMS_SR_clip.tif"
-    # dir_ROIraster = './data/ROI/ROI_20180528_181110_add.tif'
-    # dir_samples_root = './data/samples/sample_'
-    # dir_samples = 'sample_174k.csv'
-    if flag_rasterize:
-        flag_output = True
-        # rasterize ROI
-        ROI = vector_rasterize(
-            dir_vector=dir_ROI,
-            dir_raster=dir_raster,
-            dir_out=dir_ROIraster,
-            flag_output=flag_output,
-        )
+def data_training_existing(dir_samples: str) -> DataFrame:
+    """
+    Creates training data from a csv file
 
-        # save surface reflectance and label to csv file
-        N_scale = 10000.0
-        img = rasterio.open(dir_raster)
-        ROI = rasterio.open(dir_ROIraster)
-        img_read = img.read() / N_scale
-        df_img = pd.DataFrame(img_read.reshape([4, -1]).T)
-        df_label = pd.DataFrame(ROI.read().reshape([1, -1]).T)
-        df_train = pd.concat([df_img, df_label], axis=1)
-        df_train.columns = ["blue", "green", "red", "nir", "label"]
-        df_train["ndvi"] = (df_train["nir"] - df_train["red"]) / (
-            df_train["nir"] + df_train["red"]
-        )
-        df_train = df_train[df_train.label != 9]
-        df_train.label = np.where(df_train.label > 0, 1, 0)
-        dir_samples = dir_samples_root + str(int(len(df_train.index) / 1000)) + "k.csv"
-        df_train.to_csv(dir_samples, index=False)
-    else:
-        df_train = pd.read_csv(dir_samples)
+    Parameters:
+    - dir_samples: File location of the csv file
 
+    Returns:
+    - DataFrame: DataFrame of training data
+    """
+    df_train = pd.read_csv(dir_samples)
+    return df_train
+
+
+def data_training_new(dir_ROI: str, dir_raster: str, dir_ROIraster: str, dir_samples_root: str) -> DataFrame: 
+    """
+    Creates training data from scratch
+
+    Parameters:
+    - dir_ROI:
+    - dir_raster:
+    - dir_ROIraster:
+    - dir_samples_root: 
+
+    Returns:
+    - DataFrame: DataFrame of training data
+    """
+        
+    flag_output = True    
+    
+    # rasterize ROI
+    ROI = vector_rasterize(
+        dir_vector=dir_ROI,
+        dir_raster=dir_raster,
+        dir_out=dir_ROIraster,
+        flag_output=flag_output,
+    )
+
+    # save surface reflectance and label to csv file
+    N_scale = 10000.0
+    img = rasterio.open(dir_raster)
+    ROI = rasterio.open(dir_ROIraster)
+    img_read = img.read() / N_scale
+    df_img = pd.DataFrame(img_read.reshape([4, -1]).T)
+    df_label = pd.DataFrame(ROI.read().reshape([1, -1]).T)
+    df_train = pd.concat([df_img, df_label], axis=1)
+    df_train.columns = ["blue", "green", "red", "nir", "label"]
+    df_train["ndvi"] = (df_train["nir"] - df_train["red"]) / (
+        df_train["nir"] + df_train["red"]
+    )
+    df_train = df_train[df_train.label != 9]
+    df_train.label = np.where(df_train.label > 0, 1, 0)
+    dir_samples = dir_samples_root + str(int(len(df_train.index) / 1000)) + "k.csv"
+    df_train.to_csv(dir_samples, index=False)
+        
     return df_train
